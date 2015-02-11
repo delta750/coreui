defa905c1f53649e248ae77e78016a0f6b25b883
Jeff Herb <herb.jeff@gmail.com>
2015-02-11 13:17:23 -0500

    More inline-dev example of the core->app-common->page code with require.

---
+++ src/js/project/project.js
@@ -0,0 +1,25 @@
+define(['jquery', 'core'], function($, cui) {
+
+  var app = {};
+
+  app.init = function(cb) {
+
+    console.log("Application common script has just ran");
+
+    // ====
+    // Do all the common stuff here
+    // ====
+
+    // Check to see if we have custom page level code declared in callback
+    if (typeof(cb) === 'function') {
+
+      // We do, so execute it.
+      cb();
+    }
+
+  }
+
+
+  return app;
+
+});
