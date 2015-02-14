0e445a10f73ab9c5e5443da32a67889a47cf2f8e
Craig Patik <craig@patik.com>
2015-02-14 13:15:03 -0500

    Wrap setting.js in function to prevent creation of globals

--- src/js/settings.js
+++ src/js/settings.js
@@ -1,27 +1,29 @@
-// Get the required script as the reliable path
-var scripts = document.getElementById('require'),
-    src = scripts.src,
-    baseUrl = src.substring(src.indexOf(document.location.pathname), src.lastIndexOf('/cui'));
+(function() {
+    // Get the required script as the reliable path
+    var scripts = document.getElementById('require'),
+        src = scripts.src,
+        baseUrl = src.substring(src.indexOf(document.location.pathname), src.lastIndexOf('/cui'));
 
-//console.log(baseUrl);
+    //console.log(baseUrl);
 
-require.config({
-    baseUrl: baseUrl,
-    paths: {
-        // These are core libs. DO NOT CHANGE!
-        // 'jquery':     'cui/vendor/jquery-1.11.2.min',
-        'jquery':     'cui/vendor/jquery',
-        'domReady':   'cui/vendor/domReady',
-        'datepicker': 'cui/components/datepicker/datepicker',
+    require.config({
+        baseUrl: baseUrl,
+        paths: {
+            // These are core libs. DO NOT CHANGE!
+            // 'jquery':     'cui/vendor/jquery-1.11.2.min',
+            'jquery':     'cui/vendor/jquery',
+            'domReady':   'cui/vendor/domReady',
+            'datepicker': 'cui/components/datepicker/datepicker',
 
-        // Project Components Paths
-        'project': 'project/components'
-    },
-});
+            // Project Components Paths
+            'project': 'project/components'
+        },
+    });
 
 
-// Kicks off the global project scripts
-require(['domReady!', 'jquery', 'cui'], function(domReady, $, cui) {
-    // Declares the first document.ready to make sure this code runs first.
-    cui.init();
-});
+    // Kicks off the global project scripts
+    require(['domReady!', 'jquery', 'cui'], function(domReady, $, cui) {
+        // Declares the first document.ready to make sure this code runs first.
+        cui.init();
+    });
+}());
