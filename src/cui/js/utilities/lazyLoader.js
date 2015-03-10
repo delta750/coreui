8db16c1e2cc2b8fc227f7e5b4a0e0b6adb6c473f
Jeff Herb <herb.jeff@gmail.com>
2015-03-10 13:53:03 -0400

    Simplified the lazyloader for now

--- src/cui/js/utilities/lazyLoader.js
+++ src/cui/js/utilities/lazyLoader.js
@@ -6,33 +6,7 @@ define(['require'], function (require) {
   // Create the function used to load scripts inline.
   lazyLoader.load = function(request, requestCb) {
 
-    var que = {};
-
     var processor = function(request, requestCb) {
-
-      // Function to handle request que callback and executes them in the order they were recieved.
-      var requestFunctions = function(arrayCb) {
-
-        if (arrayCb.length > 0) {
-
-          var nextFunc = arrayCb[0];
-          arrayCb.shift();
-
-          // Make sure the item is a function, if not skip it.
-          if (typeof(nextFunc) === 'function') {
-            nextFunc();
-          }
-
-          requestFunctions(arrayCb);
-
-        } else {
-
-          return;
-
-        }
-
-      };
-
 
       // Check to see if the requested library is already been defined in requirejs
       if (require.defined(request)) {
@@ -44,23 +18,7 @@ define(['require'], function (require) {
 
       } else {
 
-        // Check for the item que already exits
-        if (que[request]) {
-
-          // Add request callback to existing queue
-          que[request].push(requestCb);
-
-        } else {
-
-          // Create a que for this request
-          que[request] = [requestCb];
-
-          // Request the item
-          require([request], function() {
-            requestFunctions(que[request]);
-          });
-
-        }
+          require([request], requestCb);
 
       }
 
