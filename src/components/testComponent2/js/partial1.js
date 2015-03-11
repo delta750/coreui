c96b9934f21e46c88be7a036027b5f1e6bcdf02f
Jeff Herb <herb.jeff@gmail.com>
2015-03-11 07:57:29 -0400

    removed the un-needed components

--- src/components/testComponent2/js/partial1.js
+++
@@ -1,3 +0,0 @@
-define(['partial2'], function() {
-    console.log('This is the first sub module for test component 2');
-});
--- src/components/testComponent2/js/partial2.js
+++
@@ -1,5 +0,0 @@
-define([], function($, cui) {
-
-    console.log('this is the second sub module for test component 2');
-
-});
