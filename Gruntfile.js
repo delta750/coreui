70481a18cac8597882d4d60b0139f79b1dad89b3
Craig Patik <craig.patik@its.ny.gov>
2016-06-01 16:03:07 -0400

    Create standalone docs branch

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,197 +1,39 @@
-    module.exports = function(grunt) {
-    // Banner for JavaScript files
-    // The info comes from package.json -- see http://gruntjs.com/configuring-tasks#templates for more about pulling in data from files
-    // Insert the Live Reload script
-    var liveReloadInjection =
-            '\n(function(){' +
-                'var s = document.createElement("script");' +
-                's.src="//localhost:35729/livereload.js";' +
-                'document.head.appendChild(s);' +
-            '}());';
-
-    var jsBanner = '/*! <%= pkg.title %>\n' +
-                     ' *  @description  <%= pkg.description %>\n' +
-                     ' *  @version      <%= pkg.version %>.REL<%= grunt.template.today("yyyymmdd") %>\n' +
-                     ' *  @copyright    <%= grunt.template.today("yyyy") %> ' +
-                     '<%= pkg.author.name %>\n */\n';
-
-        // This banner will appear at the top style sheets
-    var cssBanner = '@charset "utf-8";\n' + jsBanner;
-
-    // Project configuration.
+module.exports = function (grunt) {
+    // Project configuration
     grunt.initConfig({
-
-        // Expose the banners to the tasks
-        jsBanner: jsBanner,
-        cssBanner: cssBanner,
-
-        // All Grunt modules must be listed in the `package.json` file
-        pkg: grunt.file.readJSON('package.json'),
-
-        // Variables
-        prod: false,
-
         // Remove temporary development files
         // https://github.com/gruntjs/grunt-contrib-clean
         clean: {
             options: {
-                force:true
+                force: true,
             },
-            dist: [
-                'dist'
+            compiled: [
+                'assets/**/*.*',
+                'core/**/*.html',
+                'components/**/*.html',
+                'project/**/*.html',
+                '!src/**/*.html',
             ],
         },
 
-        concat: {
-            css: {
-                options: {
-                    banner: cssBanner,
-                },
-                src: ['dist/css/main.css'],
-                dest: 'dist/css/main.css',
-            },
-            js: {
-                options: {
-                    banner: jsBanner,
-                },
-                src: ['dist/js/main.js'],
-                dest: 'dist/js/main.js',
-            }
-        },
-
         copy: {
-            fonts: {
-                files: [
-                    {
-                        expand: true,
-                        cwd: 'src/cui/fonts/',
-                        src: ['**'],
-                        dest: 'dist/fonts',
-                        filter: 'isFile',
-                    },
-                    {
-                        expand: true,
-                        cwd: 'src/project/fonts/',
-                        src: ['**'],
-                        dest: 'dist/fonts',
-                        filter: 'isFile',
-                    },
-                ],
-            },
-            images: {
-                expand: true,
-                cwd: 'src/',
-                src: [
-                        'cui/images/**.{png,jpg,jpeg,gif}',
-                        'project/images/**.{png,jpg,jpeg,gif}',
-                    ],
-                dest: 'dist/images',
-                filter: 'isFile',
-                flatten: true,
-            },
-            html: {
-                files: [
-                    {
-                        expand: true,
-                        cwd: 'src/cui/html/',
-                        src: ['**/*.html'],
-                        dest: 'dist',
-                        filter: 'isFile',
-                    },
-                    {
-                        expand: true,
-                        cwd: 'src/cui/docs/',
-                        src: [
-                                '**/*.html',
-                                '!src/**/*.html',
-                            ],
-                        dest: 'dist',
-                        filter: 'isFile',
-                    },
-                    {
-                        expand: true,
-                        cwd: 'src/project/html/',
-                        src: ['**/*.html'],
-                        dest: 'dist',
-                        filter: 'isFile',
-                    },
-                ],
-            },
-            templates: {
-                expand: true,
-                cwd: 'src/project/templates',
-                src: ['**/*.html'],
-                dest: 'dist/templates',
-                filter: 'isFile',
-                flatten: true,
-            },
             docs: {
                 files: [
                     {
                         expand: true,
-                        cwd: 'src/cui/docs/src/assets/css',
+                        cwd: 'src/assets/css',
                         src: ['**/*.css'],
-                        dest: 'docs/assets/css',
+                        dest: 'assets/css',
                         filter: 'isFile',
                         flatten: true,
                     },
                     {
                         expand: true,
-                        cwd: 'src/project/docs',
+                        cwd: 'src/assets/images',
                         src: ['**/*.*'],
-                        dest: 'docs/project',
-                        filter: 'isFile',
-                    },
-                    {
-                        expand: true,
-                        cwd: 'src/cui/docs/src/assets/images',
-                        src: ['**/*.*'],
-                        dest: 'docs/assets/images',
+                        dest: 'assets/images',
                         filter: 'isFile',
                         flatten: true,
-                    },
-                    {
-                        expand: true,
-                        cwd: 'src/cui/docs/demos/',
-                        src: ['**/*.*'],
-                        dest: 'docs/demos',
-                        filter: 'isFile',
-                        flatten: true,
-                    },
-                ],
-            },
-
-            // Component documentation
-
-            modalDocs: {
-                files: [
-                    {
-                        expand: true,
-                        cwd: 'src/project/components/modal/dist/docs',
-                        src: ['**/*.*'],
-                        dest: 'docs/components/modal',
-                    },
-                ],
-            },
-
-            popoverDocs: {
-                files: [
-                    {
-                        expand: true,
-                        cwd: 'src/project/components/popover/dist/docs',
-                        src: ['**/*.*'],
-                        dest: 'docs/components/popover',
-                    },
-                ],
-            },
-
-            shortcutDocs: {
-                files: [
-                    {
-                        expand: true,
-                        cwd: 'src/project/components/shortcut/dist/docs',
-                        src: ['**/*.*'],
-                        dest: 'docs/components/shortcut',
                     },
                 ],
             },
@@ -209,35 +51,6 @@
             },
         },
 
-        // https://github.com/gruntjs/grunt-contrib-jshint
-        // Supported options: http://jshint.com/docs/
-        // Help with debugging common error messages: http://jslinterrors.com/
-        // Basic hinting is provided for the projectjs. Additional hinting should be setup
-        // the supporting component folders manually.
-        jshint: {
-            main: {
-                options: {
-                    curly: true,
-                    eqeqeq: true,
-                    browser: true,
-                    unused: 'vars',
-                    undef: true,
-                    unused: true,
-                    strict: false,
-                    globals: {
-                        jQuery: false,
-                        define: false,
-                        require: false,
-                        emp: false,
-                        fwData: false,
-                    },
-                },
-                src: [
-                    'src/project/js/**/*.js',
-                ],
-            },
-        },
-
         md2html: {
             options: {
                 highlightjs: {
@@ -248,116 +61,15 @@
             },
             docs: {
                 options: {
-                    layout: 'src/cui/docs/src/assets/templates/default.html',
+                    layout: 'src/assets/templates/default.html',
                 },
                 files: [{
                     expand: true,
-                    cwd: 'src/cui/docs/src',
+                    cwd: 'src',
                     src: ['**/*.md'],
-                    dest: 'docs',
+                    dest: '',
                     ext: '.html',
                 }],
-            },
-            external: {
-                options: {
-                    layout: 'src/cui/docs/src/assets/templates/external.html',
-                },
-                files: [{
-                    expand: true,
-                    cwd: 'src/cui/docs/src',
-                    src: ['**/*.md'],
-                    dest: 'docs',
-                    ext: '.html',
-                }],
-            },
-        },
-
-        // Builds the default javascript CUI library using r.js compiler
-        requirejs: {
-            main: {
-                options: {
-                    baseUrl: 'src/', // Where all our resources will be
-                    name: '../tasks/libs/requireManager/temp/settings', // Where the generated temp file will be
-                    paths: {}, // Generate build file
-                    include: [
-                        "requirejs",
-                        "css",
-                        "text",
-                        "json",
-                        "domReady",
-                        "lazyLoader",
-                        "jquery",
-                        "cui"
-                    ],
-                    optimize: 'none', // 'uglify2',
-                    generateSourceMaps: true,
-                    preserveLicenseComments: false,
-                    out: 'dist/js/main.js', // Where the final project will be output
-                },
-            },
-        },
-
-        // https://github.com/sindresorhus/grunt-sass
-        sass: {
-            main: {
-                options: {
-                    sourceMap: true,
-                    outputStyle: 'nested', // Options: "nested", "compressed" (i.e. minified)
-                },
-                files: {
-                    'dist/css/main.css': 'src/project/scss/project.scss',
-                },
-            },
-        },
-
-        // SVG optimization
-        // https://github.com/sindresorhus/grunt-svgmin
-        svgmin: {
-            options: {
-                plugins: [
-                    // Full list of plugins that can be disabled: https://github.com/svg/svgo/tree/master/plugins
-                    // To disable one, add a new object to this array, e.g. `{pluginName: false}`
-                    { removeUselessStrokeAndFill: false },  // don't remove Useless Strokes and Fills
-                ],
-            },
-            dist: {
-                files: [
-                    {
-                        expand: true,
-                        cwd: 'src/',
-                        src: [
-                                'cui/images/**.svg',
-                                'project/images/**.svg'
-                            ],
-                        dest: 'dist/images',
-                        filter: 'isFile',
-                        flatten: true,
-                    },
-                ],
-            },
-        },
-
-        // https://github.com/gruntjs/grunt-contrib-uglify
-        usebanner: {
-            cssBanner: {
-                options: {
-                    position: 'top',
-                    banner: cssBanner,
-                    linebreak: true
-                },
-                files: {
-                    src: [ 'dist/css/components/**/*.css' ],
-                },
-            },
-            jsBanner: {
-                options: {
-                    position: 'top',
-                    banner: jsBanner,
-                    linebreak: true
-                },
-                files: {
-                    src: [ 'dist/js/components/**/*.js' ],
-                },
             },
         },
 
@@ -368,151 +80,56 @@
                 spawn: false
             },
 
-            // Task is used with development builds to keep the connect server running.
+            // Docs
+            docs: {
+                files: [
+                    'src/**/*.*',
+                ],
+                tasks: [
+                    'md2html',
+                    'copy',
+                ],
+            },
+
+            // Task is used with development builds to keep the connect server running
             noop: {
                 files: [
                     'README.md',
                 ],
             },
-
-            // Project styles
-            styles: {
-                files: [
-                        'src/cui/scss/**/*.scss',
-                        'src/project/scss/**/*.scss',
-                       ],
-                tasks: ['sass:main'],
-            },
-
-            // Project HTML
-            html: {
-                files: ['src/project/html/**/*.html'],
-                tasks: ['copy:html'],
-            },
-
-            // CUI docs
-            cuiDocs: {
-                files: [
-                    'src/cui/docs/**/*.*',
-                    'src/project/docs/**/*.*',
-                ],
-                tasks: ['copy:docs'],
-            },
         },
-
     });
 
-    ////////////////
-    // Main tasks //
-    ////////////////
+    ///////////
+    // Tasks //
+    ///////////
 
-    // Load npm based tasks
+    // Load all Grunt tasks
     require('load-grunt-tasks')(grunt);
 
-    // Load local tasks in the task folder.
-    grunt.loadTasks('tasks');
-
-    grunt.registerTask('prod', 'Production', function (args) {
-
-        // Change some setting to optimize the build
-
-        // =================
-        // == Build Flag ===
-        // =================
-        grunt.config.set('prod', true);
-
-        // ===========
-        // == SASS ===
-        // ===========
-        // var sass = grunt.config.get('sass');
-
-        // sass.options.sourceMap = false;
-        // sass.options.outputStyle = "compressed";
-
-        // grunt.config.set('sass', sass);
-
-        // ================
-        // == RequireJS ===
-        // ================
-
-        // var requireJS = grunt.config.get('requirejs');
-
-        // requireJS.compile.options.generateSourceMaps = false;
-        // requireJS.compile.options.optimize = "uglify2";
-
-        // grunt.config.set('requirejs', requireJS);
-
-        // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-        // Other tasks like uglify and cssmin are handled by the requireManager build process.
-        // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-
-        grunt.task.run([
-            'clean',
-            'md2html:docs', // Comment this out when you dont need the Getting Started docs any longer
-            'componentFinder',
-            'copy',
-            'svgmin',
-            'sass',
-            'requirejs',
-            'concat',
-            'copy',
-            'usebanner',
-        ]);
-    });
-
-    // Alias for production build
-    grunt.registerTask('dist', 'prod');
-
-    // Development: compile script and styles, start a local server, and watch for file changes
-    // Only use this for local development
+    // Development
     grunt.registerTask('dev', 'Development', function (args) {
-        // Dynamically alter tasks when dev is called specifically
-
-        // Get Concat settings
-        var concatObj = grunt.config.get('concat');
-
-        // Add liveload to the footer
-        concatObj.js.options['footer'] = liveReloadInjection;
-
-        // Update concat
-        grunt.config.set('concat', concatObj);
-
-        // Run the development build process
-        grunt.task.run([
-            'clean',
-            'md2html:docs', // Comment this out when you dont need the Getting Started docs any longer
-            'componentFinder',
-            'copy',
-            'svgmin',
-            'sass',
-            'requirejs',
-            'concat',
-            'copy',
-            'usebanner',
-            'watch',
-            'connect',
-        ]);
-    });
-
-    // Documentation for an external site
-    grunt.registerTask('docs', 'Documentation', function (args) {
         grunt.task.run([
             'clean',
             'md2html',
-            'componentFinder',
-            // 'svgmin',
-            'sass',
-            'requirejs',
             'copy',
-            'clean', // Remove `dist` folder which is no longer needed
-            // 'concat',
-            // 'usebanner',
+            'connect',
+            'watch',
+        ]);
+    });
+
+    // Build documentation
+    grunt.registerTask('dist', 'Build documentation', function (args) {
+        grunt.task.run([
+            'clean',
+            'md2html',
+            'copy',
         ]);
     });
 
     // Simple web server
     grunt.registerTask('server', 'Server', ['connect', 'watch:noop']);
 
-    // Set the default task to the development build
+    // Set the default task to the documentation build
     grunt.registerTask('default', 'dev');
 };
---
+++ src/core/index.md
@@ -0,0 +1,37 @@
+# Introduction to Core UI
+
+Core UI is a toolkit for easy development of web applications and sites. It is platform-agnostic and built on the principles of modularity, flexibility, and scalability. Core UI is developed by the New York State Office for Information Technology Services.
+
+# Features
+
++ Platform- and CMS-agnostic
+    - Works well with Drupal, TeamSite, or any other platform including custom frameworks
++ No overbearing defaults
+    - Core UI boosts your project's styles and scripts instead of enforcing its own
++ Componentized and modular
+    - Projects are collections of modules that can be added and customized as needed, per-app and per-page
+    - Your project seamlessly inherits the Core UI framework
++ Easily customizable
+    - Can be used to design a multitude of pages with dramatically different
+content and functionality
+    - Offers a solid framework for you to build your custom plugins or use third party tools directly with the framework
++ Responsive
+    - Supports a wide variety of devices without special coding or add-ons
++ Ease of standardization
+    - Use the same components across all projects in your web environment
+
+## Comparison to other toolkits
+
++ No enforced styles
+    - You don't need to overwrite Core UI's styles with your own and duplicate the amount of CSS you're delivering to the user
++ Linear design promotes speed and ease of integration
+    - The toolkit inherits your styles rather than imposing styles that you must override:
+    ![](/assets/images/cuiLinear.jpg)
+    ![](/assets/images/iterate.jpg)
++ Any component can be bundled with your project or loaded conditionally
++ Core UI can have pages styled independently from each other with no global styles or formatting
+
+## What's next
+
++ Option styles adhering to the NY.gov branding guidelines
++ Wider selection of components
